76d55ffc03ff74ffcd6d1a81a4048ba9b8b51d43
choimake <choimake330@gmail.com>
2020-08-09 10:30:33 +0900

    meta情報用のcomponent作成

---
+++ components/meta.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Meta = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/icon-16x16.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/icon-32x32.png"></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicons/apple-touch-icon-180x180.png"
+        ></link>
+        <meta name="description" content="description"></meta>
+      </Head>
+    </>
+  );
+};
+
+export default Meta;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
+import Meta from '../components/meta';
 
 function Index(): JSX.Element {
   return (
     <>
+      <Meta></Meta>
       <Head>
         <title>Index</title>
       </Head>
